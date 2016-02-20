d92d537983d6360f08fd48b08e43b158087328ab
Adam-MainPC <maorcpa.adam@gmail.com>
2016-02-20 18:52:17 +0200

    Some more GUI: added a background - not really lovin it... fixed button colors. fixed rounded boxes for topics and answerbox. made color colors for tags. add glyphIcons everywhere. changed login page - added logo.

--- AllThatMusicGear/src/allthatmusicgear/constants/CreateTablesConstants.java
+++ AllThatMusicGear/src/allthatmusicgear/constants/CreateTablesConstants.java
@@ -1,40 +1,45 @@
 package allthatmusicgear.constants;
 
+/**
+ * @author Adam Ma'or, Vadim Xaxam
+ * 
+ * This interface holds the constants to build our tables;
+ *
+ */
 public interface CreateTablesConstants {
 	
-	
 	public final String CREATE_USER_TABLE = "CREATE TABLE app.tblUser "
-	+ "(UserName VARCHAR(10) PRIMARY KEY,"
-	+ "Password VARCHAR(8) NOT NULL,"
-	+ "NickName VARCHAR(20) UNIQUE NOT NULL,"
-	+ "Descreption	VARCHAR(50) DEFAULT '',"
-	+ "PhotoURL VARCHAR(100) DEFAULT '', "/*we will check for valid URL in javascript*/
-	+ "UserRating DOUBLE DEFAULT 0 )" ;
+			+ "(UserName VARCHAR(10) PRIMARY KEY,"
+			+ "Password VARCHAR(8) NOT NULL,"
+			+ "NickName VARCHAR(20) UNIQUE NOT NULL,"
+			+ "Descreption	VARCHAR(50) DEFAULT '',"
+			+ "PhotoURL VARCHAR(100) DEFAULT '', "
+			+ "UserRating DOUBLE DEFAULT 0 )" ;
 		
 	public final String CREATE_QUESTION_TABLE = "CREATE TABLE app.tblQuestion"
-	+ "(QID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
-	+ "QUNickName VARCHAR(20) NOT NULL,"
-	+ "QText VARCHAR(300) NOT NULL,"
-	+ "QSubmissionTime TIMESTAMP DEFAULT TIMESTAMP(CURRENT_DATE,CURRENT_TIME),"
-	+ "QVotingScore INT DEFAULT 0,"
-	+ "QRating DOUBLE DEFAULT 0,"
-	+ "FOREIGN KEY(QUNickName) REFERENCES app.tblUser(NickName))";
+			+ "(QID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
+			+ "QUNickName VARCHAR(20) NOT NULL,"
+			+ "QText VARCHAR(300) NOT NULL,"
+			+ "QSubmissionTime TIMESTAMP DEFAULT TIMESTAMP(CURRENT_DATE,CURRENT_TIME),"
+			+ "QVotingScore INT DEFAULT 0,"
+			+ "QRating DOUBLE DEFAULT 0,"
+			+ "FOREIGN KEY(QUNickName) REFERENCES app.tblUser(NickName))";
 	
 	public final String CREATE_QUESTION_TOPIC_TABLE = "CREATE TABLE app.tblQuestionTopics"
-	+ "(QID INT,"
-	+ "Topic VARCHAR(50),"
-	+ "PRIMARY KEY (QID, Topic),"
-	+ "FOREIGN KEY (QID) REFERENCES app.tblQuestion(QID))";
+			+ "(QID INT,"
+			+ "Topic VARCHAR(50),"
+			+ "PRIMARY KEY (QID, Topic),"
+			+ "FOREIGN KEY (QID) REFERENCES app.tblQuestion(QID))";
 	
 	public final String CREATE_ANSWER_TABLE = "CREATE TABLE app.tblAnswer" 
-	+ "(AID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
-	+ "QuestionID INT NOT NULL,"
-	+ "AUNickName VARCHAR(20) NOT NULL,"
-	+ "AText VARCHAR(300) NOT NULL,"
-	+ "ASubmissionTime TIMESTAMP DEFAULT TIMESTAMP(CURRENT_DATE,CURRENT_TIME),"
-	+ "AVotingScore INT DEFAULT 0,"
-	+ "FOREIGN KEY(QuestionID) REFERENCES app.tblQuestion(QID),"
-	+ "FOREIGN KEY(AUNickName) REFERENCES app.tblUser(NickName))";
+			+ "(AID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
+			+ "QuestionID INT NOT NULL,"
+			+ "AUNickName VARCHAR(20) NOT NULL,"
+			+ "AText VARCHAR(300) NOT NULL,"
+			+ "ASubmissionTime TIMESTAMP DEFAULT TIMESTAMP(CURRENT_DATE,CURRENT_TIME),"
+			+ "AVotingScore INT DEFAULT 0,"
+			+ "FOREIGN KEY(QuestionID) REFERENCES app.tblQuestion(QID),"
+			+ "FOREIGN KEY(AUNickName) REFERENCES app.tblUser(NickName))";
 	
 	public final String CREATE_QUESTION_VOTES_TABLE = "CREATE TABLE app.tblQuestionVotes"
 			+ "(QID INT, UNickName VARCHAR(20), Vote INT, "
