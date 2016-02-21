41c9fa4f0a76e9c1f2cc7eccfdcc929a29212365
Adam-MainPC <maorcpa.adam@gmail.com>
2016-02-21 14:41:04 +0200

    Changed Listener Name

--- AllThatMusicGear/src/allthatmusicgear/listeners/ServerListener.java
+++ AllThatMusicGear/src/allthatmusicgear/listeners/ServerListener.java
@@ -11,29 +11,27 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextAttributeEvent;
-import javax.servlet.ServletContextAttributeListener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 
 import allthatmusicgear.constants.CreateTablesConstants;
-import allthatmusicgear.constants.DBConstants;;
+import allthatmusicgear.constants.DBConstants;
+
 /**
- * Application Lifecycle Listener implementation class LogAndRegListener
+ * Application Lifecycle Listener implementation class ServerListener
  *
  */
-public class LogAndRegListener implements ServletContextListener, ServletContextAttributeListener {
+public class ServerListener implements ServletContextListener {
 
     /**
      * Default constructor. 
      */
-    public LogAndRegListener() {
-        // TODO Auto-generated constructor stub
+    public ServerListener() {
     }
     
-    //utility that checks whether the customer tables already exists
+  //utility that checks whether the customer tables already exists
     private boolean tableAlreadyExists(SQLException e) {
         boolean exists;
         if(e.getSQLState().equals("X0Y32")) {
@@ -43,34 +41,31 @@ public class LogAndRegListener implements ServletContextListener, ServletContext
         }
         return exists;
     }
-    
+
 	/**
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
     public void contextInitialized(ServletContextEvent event)  { 
-         // TODO Auto-generated method stub
     	System.setProperty("derby.system.home", "c:/Web/DerbyDBFolder");
     	ServletContext cntx = event.getServletContext();
+    	Connection conn = null;
     	
     	try{
     		
     		//obtain CustomerDB data source from Tomcat's context
     		Context context = new InitialContext();
     		BasicDataSource ds = (BasicDataSource)context.lookup(DBConstants.DB_DATASOURCE);
-    		Connection conn = ds.getConnection();
-    		
-    		
-    		
+    		conn = ds.getConnection();  		
     		List<String> dmlList = new ArrayList<String>();
  
 //    		If you want to remove all the tables uncomment this and comment the next 4 Lines - remeber to comment them again
     		
-//    		dmlList.add("DROP TABLE app.tblQuestionVotes");
-//    		dmlList.add("DROP TABLE app.tblAnswerVotes");
-//    		dmlList.add("DROP TABLE app.tblAnswer");
-//    		dmlList.add("DROP TABLE app.tblQuestionTopics");
-//    		dmlList.add("DROP TABLE app.tblQuestion");
-//    		dmlList.add("DROP TABLE app.tblUser");
+    		dmlList.add("DROP TABLE app.tblQuestionVotes");
+    		dmlList.add("DROP TABLE app.tblAnswerVotes");
+    		dmlList.add("DROP TABLE app.tblAnswer");
+    		dmlList.add("DROP TABLE app.tblQuestionTopics");
+    		dmlList.add("DROP TABLE app.tblQuestion");
+    		dmlList.add("DROP TABLE app.tblUser");
     		
     		
     		dmlList.add(CreateTablesConstants.CREATE_USER_TABLE);
@@ -95,32 +90,41 @@ public class LogAndRegListener implements ServletContextListener, ServletContext
     				//in the past
     				created = tableAlreadyExists(e);
     				if (!created){
+    					if (conn != null) {
+    	    				conn.close();
+    	    			}
     					throw e;//re-throw the exception so it will be caught in the
     					//external try..catch and recorded as error in the log
     				}
     			}
     		}
-    		
-    		//close connection
-    		conn.close();
 
-    	} catch (/*IOException | */SQLException | NamingException e) {
+    	} catch (SQLException | NamingException e) {
     		//log error 
     		cntx.log("Error during database initialization",e);
+    	} finally {
+    		try {
+    			if (conn != null) {
+    				conn.close();
+    			}    			
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
     	}
     }
-    
-    /**
+
+	/**
      * @see ServletContextListener#contextDestroyed(ServletContextEvent)
      */
     public void contextDestroyed(ServletContextEvent event)  { 
-         // TODO Auto-generated method stub
-    	 try { 
+    	try { 
 			DriverManager.getConnection(DBConstants.PROTOCOL + DBConstants.DB_NAME +";shutdown=true");
  		} catch (SQLException e) {
- 			if (e.getSQLState().equals("XJ015")){
+ 			String state = e.getSQLState();
+ 			if (state.equals("XJ015") || state.equals("08006")){
+ 				// if one of these errors - it ok - no need to print the exception details
+ 				// garbage collector
  				System.gc();
- 				System.out.println("Shut down success");
  			}
  			else {
  				ServletContext cntx = event.getServletContext();
@@ -128,30 +132,5 @@ public class LogAndRegListener implements ServletContextListener, ServletContext
  			}
  		}
     }
-
-	/**
-     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
-     */
-    public void attributeAdded(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
-     */
-    public void attributeRemoved(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	
-
-	/**
-     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
-     */
-    public void attributeReplaced(ServletContextAttributeEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-
 	
 }
